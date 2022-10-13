4432c3ca90dc54cd5b6f4fb32c1994eb59269c6f
bestnub <jonas.hoefler@gmail.com>
2022-10-13 03:05:43 +0200

    remove log

--- scanner.py
+++ scanner.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
                 mas.scan(ip_range, ports='25565',
                          arguments='--max-rate 300000 --excludefile exclude.conf')
                 scan_result = json.loads(mas.scan_result)
-                print(scan_result["scan"])
+                # print(scan_result["scan"])
                 for ip in scan_result["scan"]:
                     host = scan_result["scan"][ip]
                     if "tcp" == host[0]["proto"] and 25565 == host[0]["port"]:
