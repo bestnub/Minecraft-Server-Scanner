2db5e0c40b14215d354ca6f77f3fae019dc31c6e
bestnub <jonas.hoefler@gmail.com>
2022-10-13 02:20:15 +0200

    Remove try

--- scanner.py
+++ scanner.py
@@ -21,23 +21,23 @@ if __name__ == "__main__":
         random.shuffle(ip_ranges)
         for ip_range in ip_ranges:
             print(ip_range)
-            try:
-                mas = masscan.PortScanner()
-                mas.scan(ip_range, ports='25565',
+            # try:
+            mas = masscan.PortScanner()
+            mas.scan(ip_range, ports='25565',
                          arguments='--max-rate 300000 --excludefile exclude.conf')
-                print("eval")
-                for ip in mas.scan_result['scan']:
-                    host = mas.scan_result['scan'][ip]
-                    print(f"{ip} {host}")
-                    if 'tcp' in host and 25565 in host['tcp']:
-                        try:
-                            server = JavaServer(ip, 25565)
-                            status = server.status()
-                            print(status)
-                        except:
-                            print("Failed to get status of: " + ip)
+            print("eval")
+            for ip in mas.scan_result['scan']:
+                host = mas.scan_result['scan'][ip]
+                print(f"{ip} {host}")
+                if 'tcp' in host and 25565 in host['tcp']:
+                    try:
+                        server = JavaServer(ip, 25565)
+                        status = server.status()
+                        print(status)
+                    except:
+                        print("Failed to get status of: " + ip)
 
-            except:
-                print(f"{ip_range} masscan error")
+            # except:
+                # print(f"{ip_range} masscan error")
             time.sleep(30)
         print("done scanning")
