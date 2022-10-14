29caa17ab74e88b25cae16fc6c6d3f04778867cd
bestnub <63472070+bestnub@users.noreply.github.com>
2022-10-14 18:52:35 +0200

    Multi port

--- scanner.py
+++ scanner.py
@@ -7,7 +7,7 @@ import queue
 import requests
 import time
 
-ipQ = queue.LifoQueue()
+ipPortQ = queue.LifoQueue()
 sendQ = queue.LifoQueue()
 
 
@@ -47,31 +47,35 @@ class scanThread(threading.Thread):
     def run(self):
         print("Starting Thread " + self.name)
         while True:
-            print_time(self.name, ipQ.get())
+            print_time(self.name, ipPortQ.get())
 
 
 exitFlag = 0
 
 
-def print_time(threadName, ip):
+def print_time(threadName, ipPort):
+    ip = ipPort["ip"]
+    port = ipPort["port"]
+
     if exitFlag:
         print("Exiting Thread " + threadName)
         threadName.exit()
     try:
-        server = JavaServer(ip, 25565)
+        server = JavaServer(ip, port)
         status = server.status()
     except:
         print("Failed to get status of: " + ip)
     else:
-        print("Found server: " + ip + " " + status.version.name + " " +
-              str(status.players.online))
+        print("Found server: " + ip + ":" + port + " " + status.version.name +
+              " " + str(status.players.online))
         players = []
         if status.players.sample is not None:
             for player in status.players.sample:
                 players.append({'id': player.id, 'name': player.name})
+        ipPortString = ip + ":" + port
         serverScan = {
             'players': players,
-            'ip': ip,
+            'ip': ipPortString,
             'desc': status.description,
             'maxPlayer': status.players.max,
             'versionProtocol': status.version.protocol,
@@ -106,7 +110,7 @@ if __name__ == "__main__":
         for i in range(len(ip_ranges)):
             progress = round(i / len(ip_ranges) * 100)
             print(
-                f"IpRange: {ip_range} | Progress: {progress}% | Check queue: {ipQ.qsize()} | Send queue: {sendQ.qsize()}"
+                f"IpRange: {ip_range} | Progress: {progress}% | Check queue: {ipPortQ.qsize()} | Send queue: {sendQ.qsize()}"
             )
             try:
                 mas = masscan.PortScanner()
@@ -118,9 +122,9 @@ if __name__ == "__main__":
                 for ip in scan_result["scan"]:
                     host = scan_result["scan"][ip]
                     if "tcp" == host[0]["proto"] and 25565 == host[0]["port"]:
-                        ipQ.put(ip)
+                        ipPortQ.put({"ip": ip, "port": host[0]["port"]})
 
             except OSError:
                 print(f"{ip_range} masscan error")
-        ipQ.join()
+        ipPortQ.join()
         print("done scanning")
